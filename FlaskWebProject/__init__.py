1bf29468bc808eb90748655c89d5bd990540ea31
Samson Akinpade <40008759+akinpadeas@users.noreply.github.com>
2025-01-04 17:40:22 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -7,6 +7,8 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_session import Session
+from flask_wtf.csrf import CSRFProtect
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -20,5 +22,6 @@ Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
+csrf = CSRFProtect(app)
 
 import FlaskWebProject.views
