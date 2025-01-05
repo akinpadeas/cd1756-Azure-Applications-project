3e1ea4612696d38ddcef410955a6a1a0aa96a453
Samson Akinpade <40008759+akinpadeas@users.noreply.github.com>
2025-01-04 22:14:06 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -12,6 +12,10 @@ from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 app.config.from_object(Config)
+#app.secret_key = os.getenv('SECRET_KEY', '')
+app.secret_key = b'\xa2\xd3\xfb\xe5\x13\x18\xccA\xd5k\xd7\xb0\xfcH\xe3\xb9\xa0A\xdc#~\x9f\xe5'
+
+csrf = CSRFProtect(app)
 # TODO: Add any logging levels and handlers with app.logger
 streamHandler=logging.StreamHandler()
 streamHandler.setLevel(logging.WARNING)
@@ -22,6 +26,6 @@ Session(app)
 db = SQLAlchemy(app)
 login = LoginManager(app)
 login.login_view = 'login'
-csrf = CSRFProtect(app)
+
 
 import FlaskWebProject.views
