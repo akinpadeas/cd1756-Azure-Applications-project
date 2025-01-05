8703f441bdb4927a590d6156d21f4977b765270a
Samson Akinpade <40008759+akinpadeas@users.noreply.github.com>
2025-01-04 22:39:44 -0600

    Update __init__.py

--- FlaskWebProject/__init__.py
+++ FlaskWebProject/__init__.py
@@ -1,6 +1,7 @@
 """
 The flask application package.
 """
+import os
 import logging
 from flask import Flask
 from config import Config
@@ -13,7 +14,8 @@ from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 app.config.from_object(Config)
 #app.secret_key = os.getenv('SECRET_KEY', '')
-app.secret_key = b'\xa2\xd3\xfb\xe5\x13\x18\xccA\xd5k\xd7\xb0\xfcH\xe3\xb9\xa0A\xdc#~\x9f\xe5'
+app.secret_key=os.getenv("CLIENT_SECRET")
+#app.secret_key = b'\xa2\xd3\xfb\xe5\x13\x18\xccA\xd5k\xd7\xb0\xfcH\xe3\xb9\xa0A\xdc#~\x9f\xe5'
 
 csrf = CSRFProtect(app)
 # TODO: Add any logging levels and handlers with app.logger
